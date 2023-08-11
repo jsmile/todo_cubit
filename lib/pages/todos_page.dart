e55070951d938d99d0ca7c88f44efce91224e4b8
jsmile <jsmile@naver.com>
2023-08-11 15:13:29 +0900

    basic TodoHeader page widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/active_todo_count/active_todo_count_cubit.dart';
 
 class TodosPage extends StatelessWidget {
   const TodosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return const SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 40.0,
+            ),
+            child: Column(
+              children: [
+                TodoHeader(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'TODO',
+          style: TextStyle(
+            fontSize: 40.0,
+          ),
+        ),
+        // BlocBuilder<ActiveTodoCountCubit, ActiveTodoCountState>(
+        //   builder: (context, state) {
+        //     return Text(
+        //       '${state.activeTodoCount} items left',
+        //       style: TextStyle(color: Colors.redAccent, fontSize: 20.0),
+        //     );
+        //   },
+        // ),
+        Text(
+          '${context.watch<ActiveTodoCountCubit>().state.activeTodoCount} items left',
+          style: const TextStyle(
+            color: Colors.redAccent,
+            fontSize: 20.0,
+          ),
+        ),
+      ],
     );
   }
 }
